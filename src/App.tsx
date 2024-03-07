36833664475efa9f4caf415153edc867732207f1
EshimovShaxzod <126951571+EshimovShaxzod@users.noreply.github.com>
2024-03-07 23:12:24 +0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@radix-ui/themes";
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <Button>Click</Button>
+    </div>
+  );
+};
+
+export default App;
